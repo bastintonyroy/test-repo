9d7a800b0a844edc9a4388ebb33ebca61b910176
Tony Savarimuthu <savba95p@registry.otago.ac.nz>
2019-05-09 11:39:34 +1200

    added a new block of code with flipped coordinates

--- diamonds.R
+++ diamonds.R
@@ -6,3 +6,9 @@ ggplot(diamonds) +
   xlab("Price") + 
   ylab("Carat")
 
+ggplot(diamonds) + 
+  geom_boxplot(aes(x = price, y = carat, fill = cut)) + 
+  coord_flip() +
+  ggtitle("Carat vs. price split by cut") + 
+  xlab("Price") + 
+  ylab("Carat")
