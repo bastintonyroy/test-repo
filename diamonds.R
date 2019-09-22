4365e9b644b57fb2d85cb7b7b5efb63d11b5ebc5
Tony Savarimuthu <savba95p@registry.otago.ac.nz>
2019-09-22 20:13:08 +1200

    Revert "Added title and flipped coordinates"
    This reverts commit 4ffe30ff8e2e6168ead9012530e05cd69477a08b.

--- diamonds.R
+++ diamonds.R
@@ -3,6 +3,4 @@ library(tidyverse)
 ggplot(data = diamonds, aes(x = carat, y = price)) +
   geom_point() +
   xlab("Carat") +
-  ylab("Price") +
-  ggtitle("Carat vs. Price") +
-  coord_flip()
+  ylab("Price")
