4ffe30ff8e2e6168ead9012530e05cd69477a08b
Tony Savarimuthu <savba95p@registry.otago.ac.nz>
2019-09-22 20:09:14 +1200

    Added title and flipped coordinates

--- diamonds.R
+++ diamonds.R
@@ -3,4 +3,6 @@ library(tidyverse)
 ggplot(data = diamonds, aes(x = carat, y = price)) +
   geom_point() +
   xlab("Carat") +
-  ylab("Price")
+  ylab("Price") +
+  ggtitle("Carat vs. Price") +
+  coord_flip()
