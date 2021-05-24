6a9b4aefaeedc2ea8c209b402dcd68b0cf0c2c8d
REGISTRY\savba95p <bastintonyroy@gmail.com>
2021-05-24 12:08:33 +1200

    Added title, x- and y-axis labels

--- test7.R
+++ test7.R
@@ -4,4 +4,7 @@ library(nycflights13)
 flights %>% 
   count(month) %>% 
   ggplot(aes(x = as_factor(month), y = n)) +
-  geom_col()
+  geom_col() +
+  ggtitle("Counts of flights per month") +
+  xlab("Month") +
+  ylab("Count")
