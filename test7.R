f5da7f5622d1dc991ea504f84fdff87f04c385c9
REGISTRY\savba95p <bastintonyroy@gmail.com>
2021-05-24 12:14:11 +1200

    Revert "Added title, x- and y-axis labels"
    This reverts commit 6a9b4aefaeedc2ea8c209b402dcd68b0cf0c2c8d.

--- test7.R
+++ test7.R
@@ -4,7 +4,4 @@ library(nycflights13)
 flights %>% 
   count(month) %>% 
   ggplot(aes(x = as_factor(month), y = n)) +
-  geom_col() +
-  ggtitle("Counts of flights per month") +
-  xlab("Month") +
-  ylab("Count")
+  geom_col()
