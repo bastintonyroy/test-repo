6cf5302357c4432df3b5aa600daf3240d8f338e4
Tony Savarimuthu <savba95p@registry.otago.ac.nz>
2022-09-30 16:46:45 +1300

    Adding comments to code

--- test8.R
+++ test8.R
@@ -1,11 +1,13 @@
 library(tidyverse)
 library(nycflights13)
 
+# Creating a plot without using factors
 flights %>% 
   count(month) %>% 
   ggplot(aes(x = month, y = n)) +
   geom_col()
 
+# Creating a plot with using factors
 flights %>% 
   count(month) %>% 
   ggplot(aes(x = as_factor(month), y = n)) +
