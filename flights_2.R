9c72877e1e8e0078f63c3bd2cc7948d6a4bd8ae1
Tony Savarimuthu <savba95p@registry.otago.ac.nz>
2022-10-03 09:05:08 +1300

    Adding comments to code

--- flights_2.R
+++ flights_2.R
@@ -1,11 +1,13 @@
 library(tidyverse)
 library(nycflights13)
 
+#Without converting month into a factor
 flights %>% 
   count(month) %>% 
   ggplot(aes(x = month, y = n)) +
   geom_col()
 
+# After converting month into a factor
 flights %>% 
   count(month) %>% 
   ggplot(aes(x = as_factor(month), y = n)) +
