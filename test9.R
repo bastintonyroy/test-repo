af372dba107f1476135dd88c883dfdce69dbf5b1
bastintonyroy <bastintonyroy@gmail.com>
2023-09-23 09:34:41 +1200

    Added code for another plot

--- test9.R
+++ test9.R
@@ -4,3 +4,7 @@ diamonds %>%
   ggplot(aes(x = color, y = cut, fill = cut)) +
   geom_col() +
   theme(legend.position = "bottom")
+
+diamonds %>% 
+  ggplot(aes(x = cut, y = price, fill = cut)) +
+  geom_boxplot() 
