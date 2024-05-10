d357b668b4d94c625179ad307015dc85c0ccbcb9
bastintonyroy <bastintonyroy@gmail.com>
2024-05-10 20:29:33 +1200

    Added code to create monthwise count of flights

--- flights_4.R
+++ flights_4.R
@@ -2,7 +2,10 @@ library(tidyverse)
 library(nycflights13)
 flights
 
-flights %>% 
+flight_counts <- flights %>% 
   group_by(month, year) %>% 
   summarise(count = n())
 
+flight_counts %>% 
+  ggplot(aes(x = as.factor(month), y = count)) +
+  geom_col()
